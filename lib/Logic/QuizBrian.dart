76362ac6977e48e5c085e85f84632eabaab55a0a
jo3 <34713754+jo3ght@users.noreply.github.com>
2020-09-15 19:34:56 +0700

    Import Model of Questions

--- lib/Logic/QuizBrian.dart
+++ lib/Logic/QuizBrian.dart
@@ -1,7 +1,40 @@
 import 'package:review_1/Model/Question.dart';
 
 class QuizBrian {
+  int _questionNumber = 0;
   List<Question> questionList = [
-    Question("Ba", true),
+    Question("A woman has walked on the moon", false),
+    Question("Goldfish only have a memory of three seconds", false),
+    Question("Stephen Hawking declined a knighthood from the Queen", true),
+    Question("The highest mountain in England is Ben Nevis", false),
+    Question("The Great Wall of China is visible from space", false),
+    Question("The first tea bags were made of silk", true),
+    Question("The Statue of Liberty was a gift from France", true),
   ];
+
+  String getQuestion() {
+    return questionList[_questionNumber].questionText;
+  }
+
+  void nextQuestion() {
+    if (_questionNumber < questionList.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= questionList.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool getAnswer() {
+    return questionList[_questionNumber].answer;
+  }
+
+  void resetQuiz() {
+    _questionNumber = 0;
+  }
 }
--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,14 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:review_1/Logic/QuizBrian.dart';
 import 'package:review_1/Model/Question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 //test update
+
+QuizBrian _quizBrian = QuizBrian();
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+bool correctAnswer;
+
 class _HomeScreenState extends State<HomeScreen> {
   int score = 0;
+  int life = 3;
+  var indexOfQuestion = 0;
+
+  void reset() {
+    setState(() {
+      _quizBrian.resetQuiz();
+      score = 0;
+      life = 3;
+    });
+  }
+
+  void checkQuestion(bool userPicked) {
+    setState(() {
+      if (userPicked == correctAnswer) {
+        setState(() {
+          score++;
+        });
+      } else if (userPicked != correctAnswer) {
+        setState(() {
+          life--;
+          if (life == 0) {
+            Alert(
+              context: context,
+              type: AlertType.error,
+              title: "Fail !",
+              desc: "You are dead",
+              buttons: [
+                DialogButton(
+                  child: Text(
+                    "Play again",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                  color: Color.fromRGBO(0, 179, 134, 1.0),
+                ),
+              ],
+            ).show();
+            setState(() {
+              reset();
+            });
+          }
+        });
+      }
+      if (_quizBrian.isFinished() == true) {
+        Alert(
+          context: context,
+          type: AlertType.success,
+          title: "Done !",
+          desc: "You got $score points",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Play again",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              color: Color.fromRGBO(0, 179, 134, 1.0),
+            ),
+          ],
+        ).show();
+        setState(() {
+          reset();
+        });
+      }
+      _quizBrian.nextQuestion();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +90,19 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Container(
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text("Your score: $score"),
+                Text("Your life: $life"),
                 SizedBox(height: 32),
-                Text(
-                  "This is the question",
-                  style: TextStyle(fontSize: 30),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    _quizBrian.getQuestion(),
+                    style: TextStyle(fontSize: 30),
+                  ),
                 ),
+                SizedBox(height: 350),
                 MaterialButton(
                   child: Text(
                     "True",
@@ -34,8 +112,14 @@ class _HomeScreenState extends State<HomeScreen> {
                   minWidth: 200,
                   height: 60,
                   color: Colors.green,
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      correctAnswer = _quizBrian.getAnswer();
+                      checkQuestion(true);
+                    });
+                  },
                 ),
+                SizedBox(height: 26),
                 MaterialButton(
                   child: Text(
                     "False",
@@ -44,7 +128,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   minWidth: 200,
                   height: 60,
                   color: Colors.red,
-                  onPressed: () {},
+                  onPressed: () {
+                    correctAnswer = _quizBrian.getAnswer();
+                    checkQuestion(false);
+                  },
                 ),
               ],
             ),
